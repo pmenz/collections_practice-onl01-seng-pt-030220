1e3166a373df3ae33be3fc96e9a84391394bb63a
Pablo Menz <p.menz.r@gmail.com>
2020-03-14 01:20:45 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,59 @@
+def sort_array_asc(array)
 
+array.sort! do |a,b|
+  a<=>b end
+end
+
+def sort_array_desc(array)
+
+array.sort! do |a,b|
+  b<=>a end
+end
+
+def sort_array_char_count(array)
+
+array.sort! do |a,b|
+  a.length<=>b.length end
+end
+
+def swap_elements(array)
+  temp = array[1]
+  array[1] = array[2]
+  array[2] = temp
+  array
+end
+
+def reverse_array(array)
+  array.reverse!
+end
+
+def kesha_maker(array)
+  array.collect do |element|
+    element[2] = "$"
+    element
+  end
+end
+
+def find_a(array)
+  array.select do |element|
+    element.start_with?("a")
+end
+end
+
+def sum_array(array)
+  sum = 0 
+  array.each { |element| sum= sum+element} 
+  sum
+end
+
+def add_s(array)
+  new_array = []
+  array.each_with_index do |element, index|
+    if index==1
+      new_array << element
+      next
+    end
+   new_array << (element << "s")
+end
+new_array
+end
